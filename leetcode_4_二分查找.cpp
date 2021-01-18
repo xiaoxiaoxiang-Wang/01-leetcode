fcbbb463f9e903136d5abdb6d7d637f0b4f456f3
w00452607 <wangyouxiang2@huawei.com>
2021-01-18 17:32:32 +0800

    leetcode_4_二分查找.cpp

---
+++ leetcode_4_二分查找.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size(), n = nums2.size(), left = (m + n + 1) / 2, right = (m + n + 2) / 2;
+        return (findKth(nums1, 0, nums2, 0, left) + findKth(nums1, 0, nums2, 0, right)) / 2.0;
+    }
+    int findKth(vector<int>& nums1, int i, vector<int>& nums2, int j, int k) {
+        // k表示排在第k个的元素
+        if (i == nums1.size()) return nums2[j + k - 1];
+        if (j == nums2.size()) return nums1[i + k - 1];
+        if (k == 1) return min(nums1[i], nums2[j]);
+        int midVal1 = (i + k / 2 - 1 < nums1.size()) ? nums1[i + k / 2 - 1] : INT_MAX;
+        int midVal2 = (j + k / 2 - 1 < nums2.size()) ? nums2[j + k / 2 - 1] : INT_MAX;
+        if (midVal1 < midVal2) {
+            return findKth(nums1, i + k / 2, nums2, j, k - k / 2);
+        } else {
+            return findKth(nums1, i, nums2, j + k / 2, k - k / 2);
+        }
+    }
+};
+// 二分是指每次淘汰一个数组的k/2个，直到淘汰的k个后即找到对应要的值
